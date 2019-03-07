b00b00929b31cfddcd0d436b6f801d3b2b695ba5
Ramiro Bou <bouramiro@gmail.com>
2019-03-07 20:33:22 -0300

    Adding example config. Closes #1

---
+++ example.config.py
@@ -0,0 +1,37 @@
+# Homeserver address
+homeserver = "https://phuks.co"
+
+# Homeserver credentials. Not required if using a .token file
+username = "jenny"
+password = "nice&secure"
+
+
+# Display name
+nick = "Jenny"
+
+# Prefix for commands
+prefix = '.'
+
+# List of admins using full matrix usernames
+admins = ['@polsaker:phuks.co']
+
+# If this list is not empty, the bot will ONLY load the modules on this list
+whitelistonly_modules = []
+
+# Modules that won't be loaded
+disabled_modules = ['steam', 'phuks']
+
+# WolframAlpha API key, used for wolframalpha.py
+wolframalpha_apikey = "123456-7894561234"
+
+# Google API key, used by various modules
+google_apikey = "PutItInHere"
+
+# DarkSky API key, used for the weather module
+darksky_apikey = "GibMeTheWeather"
+
+# Cleverbot API key, used for cleverbot.py
+CLEVERBOT_API_KEY = "CccCcCccCCCccCccCc"
+
+# List of servers we will blindly join channels when invited to
+allowed_servers = ['phuks.co', 'chat.poal.co']
