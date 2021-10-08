c98d0bc3d537d7c2cfe049e3912839c62287f90b
Ramiro Bou <bouramiro@gmail.com>
2021-10-08 16:38:05 -0300

    Chat: Tweaking parameters

--- modules/chat.py
+++ modules/chat.py
@@ -65,11 +65,11 @@ async def send_chat_msg(bot: Jenny, room: MatrixRoom):
         engine="davinci",
         prompt=prompt,
         temperature=0.9,
-        max_tokens=300,
+        max_tokens=150,
         top_p=1,
-        frequency_penalty=1,
+        frequency_penalty=0.1,
         presence_penalty=0.6,
-        stop=["\n"]
+        stop=["\n", "Jenny: "]
     )
     print(prompt)
     print(response)
