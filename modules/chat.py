c6348e71ce2b5b3bbd6cb306a9d2e54e6353c19b
Ramiro Bou <bouramiro@gmail.com>
2021-10-08 16:23:20 -0300

    Increasing max tokens

--- modules/chat.py
+++ modules/chat.py
@@ -76,7 +76,7 @@ async def send_chat_msg(bot: Jenny, room: MatrixRoom):
         engine="davinci",
         prompt=prompt,
         temperature=0.9,
-        max_tokens=150,
+        max_tokens=300,
         top_p=1,
         frequency_penalty=1,
         presence_penalty=0.6,
