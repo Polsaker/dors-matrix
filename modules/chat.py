e668c93af82279fc1b4c9d84dbd39ccd06dd39f5
Your Name <you@example.com>
2023-05-12 17:56:21 +0000

    Add dynamic triggers for chat

--- modules/chat.py
+++ modules/chat.py
@@ -5,6 +5,7 @@ from nio import MatrixRoom
 
 from dors import message_hook, Jenny, HookMessage
 import config
+import time
 
 import random
 import re
@@ -23,6 +24,7 @@ HTML_ENTITIES = { 'apos': "'" }
 noun = ['ZHVjaw==', 'Y2F0', 'ZG9n', 'aHVtYW4=',]
 r_entity = re.compile(r'\|[0-9A-F#]{,4}')
 random.seed()
+last_trigger = defaultdict(lambda: 0)
 
 
 channel_histories = defaultdict(list)
@@ -41,15 +43,21 @@ async def random_chat(bot: Jenny, room: MatrixRoom, event: HookMessage):
     #     text = " ".join(text.split(" ")[1:])
 
     dn = await bot.get_displayname(event.sender)
-    if not getattr(dn, "displayname", False):
-        return
-    channel_histories[room.room_id].append(f"{dn.displayname}: {text}")
+    if not getattr(dn, "displayname", False) or len(dn.displayname) > 16:
+        dn = event.sender[1:].split(":")[0]
+    else:
+        dn = dn.displayname
+    channel_histories[room.room_id].append(f"<{dn}> {text}")
     if len(channel_histories[room.room_id]) > 15:
         channel_histories[room.room_id].pop(0)
 
     trigger = random.random() <= (1 / 2500.0) or config.nick.lower() in event.sane_body.lower() or \
               "jen" in event.sane_body.lower()
 
+    a = lambda x: min(80, (100/x)+(x**-2)*20000)
+    dice = random.randrange(0, 10000) / 100
+
+    trigger = trigger or dice < (a(time.time() - last_trigger[room.room_id]))
     if not trigger:
         return
 
@@ -61,26 +69,31 @@ async def send_chat_msg(bot: Jenny, room: MatrixRoom):
     await bot.room_typing(room.room_id, True, 10000)
     prompt = f"{config.prompt}\n\n"
     prompt += "\n".join(channel_histories[room.room_id])
-    prompt += "\nJenny:"
+    prompt += "\n<Jenny>"
+
+#    prompts = [{"role": "system", "content": config.prompt}]
+#    prompts += [{"role": "user", "content": config.prompt}]
+#    prompts += channel_histories[room.room_id]
 
     response = openai.Completion.create(
         engine="davinci",
         prompt=prompt,
         temperature=0.9,
-        max_tokens=200,
+        max_tokens=500,
         top_p=1,
-        frequency_penalty=0.1,
-        presence_penalty=0.6,
-        stop=["\n", "Jenny: "]
+        frequency_penalty=0.7,
+        presence_penalty=0.5,
+        stop=["\n", "<Jenny>"]
+#         messages=prompts
     )
     print(prompt)
     print(response)
     reply = response['choices'][0]['text'].strip()
     await bot.room_typing(room.room_id, False)
-    channel_histories[room.room_id].append(f"{config.nick}: {reply}")
-    if len(channel_histories[room.room_id]) > 15:
+    channel_histories[room.room_id].append(f"<Jenny> {reply}")
+    if len(channel_histories[room.room_id]) > 10:
         channel_histories[room.room_id].pop(0)
-
+    last_trigger[room.room_id] =  time.time()
     await bot.message(room.room_id, reply)
     if random.randint(0, 10) == 0:
         await send_chat_msg(bot, room)
